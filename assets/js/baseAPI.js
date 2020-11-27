3606c5ec179e49dc94b6dfa1e431821471c5ea65
tan <2274562300@qq.com>
2020-11-27 16:36:34 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,18 @@
 var baseURL = "http://ajax.frontend.itheima.net";
 $.ajaxPrefilter(function(params){
     params.url = baseURL + params.url;
+    if(params.url.indexOf("/my/") !== -1){
+        params.headers = {Authorization:localStorage.getItem("token") || ""}
+    }
+    //一有响应就拦截，判断身份认证信息
+    params.complete = function(res){
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if(obj.status == 1 && obj.massage == "身份认证失败"){
+            // 清空本地token
+            localStorage.removeItem("token");
+            //页面跳转
+            location.href = "/login.html"
+        }
+    }
 });
